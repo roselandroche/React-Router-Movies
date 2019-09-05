400caff5b5baa77f84007cf569c5e05bd113fc1b
roselandroche <r.landroche@gmail.com>
2019-09-04 20:19:45 -0400

    Really stuck on the dynamic route in part 1

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -5,7 +5,7 @@ const Movie = (props) => {
   const [movie, setMovie] = useState({});
  
   useEffect(() => {
-    const id = {id};
+    const id = 1;
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
@@ -17,8 +17,9 @@ const Movie = (props) => {
         .catch(error => {
           console.error(error);
         });
-
   },[]);
+  
+  // console.log(response.data);
   
   // Uncomment this only when you have moved on to the stretch goals
   // const saveMovie = () => {
