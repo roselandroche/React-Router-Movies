7030c4f14c84540b02858dd817b7d875df772c88
roselandroche <r.landroche@gmail.com>
2019-09-06 20:19:19 -0400

    Completed step 2

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -5,14 +5,12 @@ const Movie = (props) => {
   const [movie, setMovie] = useState({});
  
   useEffect(() => {
-    const id = movie.id.find(film => {
-      return film.id === props.match.params.id;
-    });
+    
     // change ^^^ that line and grab the id from the URL
     // You will NEED to add a dependency array to this effect hook
 
        axios
-        .get(`http://localhost:5000/api/movies/${id}`)
+        .get(`http://localhost:5000/api/movies/${props.match.params.id}`)
         .then(response => {
           setMovie(response.data);
         })
@@ -20,7 +18,7 @@ const Movie = (props) => {
           console.error(error);
         });
   },[]);
-
+  console.log(props)
   // Uncomment this only when you have moved on to the stretch goals
   // const saveMovie = () => {
   //   const addToSavedList = props.addToSavedList;
